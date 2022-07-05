e7fa3aac96c1f6975d3395ad6e1ecbbdcb127b8e
Tony Bowden <tony@tmtm.com>
2022-07-05 10:55:38 +0300

    update to newest approach

--- sources/eswiki/scraper.rb
+++ sources/eswiki/scraper.rb
@@ -1,74 +1,58 @@
 #!/bin/env ruby
 # frozen_string_literal: true
 
-require 'csv'
+require 'every_politician_scraper/scraper_data'
 require 'pry'
-require 'scraped'
-require 'table_unspanner'
-require 'wikidata_ids_decorator'
 
-require 'open-uri/cached'
+class MemberList
+  class Members
+    decorator RemoveReferences
+    decorator UnspanAllTables
+    decorator WikidataIdsDecorator::Links
 
-class RemoveReferences < Scraped::Response::Decorator
-  def body
-    Nokogiri::HTML(super).tap do |doc|
-      doc.css('sup.reference').remove
-    end.to_s
+    def member_container
+      noko.xpath("//table[.//th[contains(.,'Cartera')]]//tr[td]")
+    end
+  end
+
+  class Member
+    field :id do
+      name_node.css('a/@wikidata').first
+    end
+
+    field :name do
+      name_node.text.tidy
+    end
+
+    field :positionID do
+    end
+
+    field :position do
+      tds[0].text.tidy
+    end
+
+    field :startDate do
+      WikipediaDate::Spanish.new(raw_start).to_s
+    end
+
+    field :endDate do
+    end
+
+    private
+
+    def tds
+      noko.css('td')
+    end
+
+    def name_node
+      tds[1]
+    end
+
+    def raw_start
+      tds[2].text.split('-').first
+    end
   end
 end
 
-class UnspanAllTables < Scraped::Response::Decorator
-  def body
-    Nokogiri::HTML(super).tap do |doc|
-      doc.css('table.wikitable').each do |table|
-        unspanned_table = TableUnspanner::UnspannedTable.new(table)
-        table.children = unspanned_table.nokogiri_node.children
-      end
-    end.to_s
-  end
-end
-
-class MinistersList < Scraped::HTML
-  decorator RemoveReferences
-  decorator UnspanAllTables
-  decorator WikidataIdsDecorator::Links
-
-  field :ministers do
-    member_entries.map { |ul| fragment(ul => Officeholder).to_h }
-  end
-
-  private
-
-  def member_entries
-    noko.xpath('//table[.//th[contains(.,"Cartera")]][1]//tr[td]')
-  end
-end
-
-class Officeholder < Scraped::HTML
-  field :wdid do
-    tds[1].css('a/@wikidata').first
-  end
-
-  field :name do
-    tds[1].text.tidy
-  end
-
-  field :position do
-    tds[0].text.tidy
-  end
-
-  private
-
-  def tds
-    noko.css('td')
-  end
-end
-
-url = 'https://es.wikipedia.org/wiki/Mario_Abdo_Ben%C3%ADtez'
-data = MinistersList.new(response: Scraped::Request.new(url: url).response).ministers
-
-header = data.first.keys.to_csv
-rows = data.map { |row| row.values.to_csv }
-abort 'No results' if rows.count.zero?
-
-puts header + rows.join
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url).csv
